a266c714c465b44c0ead379c4abdba6520ff97bf
JulyInSummer <azimjanovbogdan@gmail.com>
2025-05-30 03:52:29 +0500

    feat: add QuoteDoesNotExistError constant to config

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -1,7 +1,13 @@
 package config
 
+import "errors"
+
 const (
 	HTTPInvalidBodyMessage          = "Invalid body. Please provide a valid JSON."
 	HTTPInvalidPathParameterMessage = "Invalid path parameter. Please provide a valid ID."
 	HTTPNotFoundMessage             = "Not found. Please provide a valid ID."
+)
+
+var (
+	QuoteDoesNotExistError = errors.New("quote does not exist")
 )
