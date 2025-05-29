58abaf2457651ee4d4ed2496aa1a88dacc41b0fd
JulyInSummer <azimjanovbogdan@gmail.com>
2025-05-30 03:31:32 +0500

    feat: add new HTTP error messages for invalid path and not found

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -1,5 +1,7 @@
 package config
 
 const (
-	HTTPInvalidBodyMessage = "Invalid body. Please provide a valid JSON."
+	HTTPInvalidBodyMessage          = "Invalid body. Please provide a valid JSON."
+	HTTPInvalidPathParameterMessage = "Invalid path parameter. Please provide a valid ID."
+	HTTPNotFoundMessage             = "Not found. Please provide a valid ID."
 )
